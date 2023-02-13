f26a597369da3e8714da243677ce7553469ea515
woobertdoo <89357676+woobertdoo@users.noreply.github.com>
2023-02-12 19:02:24 -0600

    removed jukit because it does annoying things when opening a second file in nvim-tree

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -210,11 +210,6 @@ _G.packer_plugins = {
     path = "/home/jamieharris/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
-  ["vim-jukit"] = {
-    loaded = true,
-    path = "/home/jamieharris/.local/share/nvim/site/pack/packer/start/vim-jukit",
-    url = "https://github.com/luk400/vim-jukit"
-  },
   ["vim-nand2tetris-syntax"] = {
     loaded = true,
     path = "/home/jamieharris/.local/share/nvim/site/pack/packer/start/vim-nand2tetris-syntax",
